ce44d3e640ea33d981d968169afc173c26663698
Artūrs Krapāns <arturs.krapans@gmail.com>
2018-05-01 14:50:53 +0300

    Player relation with statistics
    artukrap/table-tennis-score-dashboard#4

--- lib/table_tennis/accounts/player.ex
+++ lib/table_tennis/accounts/player.ex
@@ -3,11 +3,13 @@ defmodule TableTennis.Accounts.Player do
   import Ecto.Changeset
 
   alias TableTennis.Games.GamePlayer
+  alias TableTennis.Accounts.PlayerStatistic
 
   schema "players" do
     field(:name, :string)
 
     has_many(:games, GamePlayer)
+    has_many(:statistics, PlayerStatistic)
 
     timestamps()
   end
